c6d75388aa9232b264ce3a82751f0d1a479d925f
yq-scarlet <821103428Qqq.com>
2018-11-17 23:16:55 +0800

    rem转化

--- src/views/mall/mall.vue
+++ src/views/mall/mall.vue
@@ -2,6 +2,15 @@
     <div>
         <h1>this is mall huhdu page</h1>
         <Loading :show="show" :text="text"></Loading>
+        <p class="params">哈哈哈少有的好</p>
+        <div class="box">dewdhhhhhhhhhhhhhh</div>
+        <ul>
+            <li class="item">111</li>
+            <li class="item">222</li>
+            <li class="item">333</li>
+            <li class="item">444</li>
+        </ul>
+        <div class="div1">家居安静安静</div>
     </div>
 </template>
 
@@ -24,5 +33,12 @@
 </script>
 
 <style>
-
+    .params {
+        font-size: 20px;
+    }
+    .box {
+        width: 1rem;
+        height: 1rem;
+        background-color: #f00;
+    }
 </style>
